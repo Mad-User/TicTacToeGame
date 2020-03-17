c1501584919826a610cae8b17568f9d48f54d8ba
Mad-User <kotov.andrey01@mail.ru>
2020-03-17 11:26:45 +0300

    last 'Big commit'

---
+++ src/main/java/icc/stud/kotov_av/tictac/Cell.java
@@ -0,0 +1,16 @@
+package icc.stud.kotov_av.tictac;
+
+public enum Cell {
+    /**
+     * Поле 'Нолик' 
+     */
+    O,
+    /**
+     * Поле 'Крестик' 
+     */
+    X,
+    /**
+     * Поле 'Пусто' 
+     */
+    EMPTY;
+}
---
+++ src/main/java/icc/stud/kotov_av/tictac/TicTacToe.java
@@ -0,0 +1,109 @@
+package icc.stud.kotov_av.tictac;
+
+public class TicTacToe {
+    
+    private Cell[][] field;
+    private final int size;
+
+    /**
+     * @param size The size of the field
+     */
+    public TicTacToe(int size) {
+        this.size = size;
+        field = new Cell[size][size];
+        
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+        	field[i][j] = Cell.EMPTY;
+            }
+        }
+    }
+
+    /**
+     * Getting the contents of a field the coordinates
+     * 
+     * @param x X coordinate
+     * @param y Y coordinate
+     * @return Returns the contents of the cell (x, y)
+     */
+    public Cell get(int x, int y) {
+        return field[x][y];
+    }
+    
+    /**
+     * Writes X or Y to the specified cell
+     * 
+     * @param x X coordinate
+     * @param y Y coordinate
+     * @param type Recordable condition
+     * @return Notifies of successful (0) or unsuccessful (-1) completion of the record
+     */ 
+    public int write(int x, int y, Cell type) {
+        if (field[x][y] == Cell.EMPTY && (type != Cell.O || type != Cell.X)) {
+            field[x][y] = type;
+            return 0;
+        } else return -1;
+    }
+
+    /**
+     * Clearing the specified cell
+     * 
+     * @param x X coordinate
+     * @param y Y coordinate
+     */
+    public void clear(int x, int y) {
+	field[x][y] = Cell.EMPTY;
+    }
+
+    /**
+     * Finds the maximum length of a sequence of values of the specified type
+     * 
+     * @param type Type of cells to search for
+     * @return Array: maximum length of the sequence, coordinates of the beginning and end of the sequence
+     */
+    public int[] findMaxLength(Cell type) {
+        int maxLength = 0;
+        int lastMaxLength = 0;
+
+        // search by lines
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (field[i][j] == type) lastMaxLength++;
+                else {
+                    if (maxLength < lastMaxLength) maxLength = lastMaxLength;
+                    lastMaxLength = 0;
+                }
+            }
+        }
+        
+        // search by columns
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (field[j][i] == type) lastMaxLength++;
+                else {
+                    if (maxLength < lastMaxLength) maxLength = lastMaxLength;
+                    lastMaxLength = 0;
+                }
+            }
+        }
+
+        // search by diagonals, parallel to the main diagonal
+        for (int x = 0; x < size; x++) {
+            
+        }
+
+        // search by diagonals, parallel to the side diagonal
+        for (int x = (size - 1); x >= 0; x--) {
+            
+        }
+
+        int[] result = new int[5];
+        result[0] = maxLength; // the maximum length of the sequence
+        result[1] = 1; // X coordinate of the beginning of the sequence
+        result[2] = 1; // Y coordinate of the beginning of the sequence
+        result[3] = 1; // X coordinate of the end of the sequence
+        result[4] = 1; // Y coordinate of the end of the sequence
+        
+        return result;
+    }
+}
