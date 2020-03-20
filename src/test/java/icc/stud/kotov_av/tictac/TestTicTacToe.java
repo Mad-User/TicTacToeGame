a44371ce768cea07e6f88176fee6b085b3fe2c37
Mad-User <kotov.andrey01@mail.ru>
2020-03-20 20:02:07 +0300

    tests for findMaxLenth() is done

--- src/test/java/icc/stud/kotov_av/tictac/TestTicTacToe.java
+++ src/test/java/icc/stud/kotov_av/tictac/TestTicTacToe.java
@@ -41,5 +41,35 @@ public class TestTicTacToe {
         assertArrayEquals(expected, tObj.findMaxLength(Cell.X));
         tObj.clearAll();
 
+        tObj.write(0, 0, Cell.O);
+        tObj.write(1, 1, Cell.O);
+        tObj.write(1, 0, Cell.X);
+        tObj.write(2, 1, Cell.X);
+        tObj.write(3, 2, Cell.X);
+        expected = new int[] {2, 0, 0, 1, 1};
+        assertArrayEquals(expected, tObj.findMaxLength(Cell.O));
+        expected = new int[] {3, 1, 0, 3, 2};
+        assertArrayEquals(expected, tObj.findMaxLength(Cell.X));
+        tObj.clearAll();
+
+        tObj.write(0, 0, Cell.O);
+        tObj.write(0, 1, Cell.O);
+        tObj.write(1, 1, Cell.O);
+        tObj.write(1, 2, Cell.O);
+        tObj.write(2, 3, Cell.O);
+        tObj.write(1, 0, Cell.X);
+        tObj.write(2, 0, Cell.X);
+        tObj.write(3, 0, Cell.X);
+        tObj.write(4, 0, Cell.X);
+        tObj.write(3, 1, Cell.X);
+        tObj.write(2, 2, Cell.X);
+        tObj.write(1, 3, Cell.X);
+        tObj.write(0, 4, Cell.X);
+        expected = new int[] {3, 0, 1, 2, 3};
+        assertArrayEquals(expected, tObj.findMaxLength(Cell.O));
+        expected = new int[] {5, 4, 0, 0, 4};
+        assertArrayEquals(expected, tObj.findMaxLength(Cell.X));
+        tObj.clearAll();
+
     }
 }
