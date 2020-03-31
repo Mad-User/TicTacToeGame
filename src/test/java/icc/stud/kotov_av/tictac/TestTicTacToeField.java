96b48533a3fbabcb6f5d7c320066aa359cfd46e0
Mad-User <kotov.andrey01@mail.ru>
2020-03-31 15:57:52 +0300

    add test stability of algorithm: randomFindWinner

--- src/test/java/icc/stud/kotov_av/tictac/TestTicTacToeField.java
+++ src/test/java/icc/stud/kotov_av/tictac/TestTicTacToeField.java
@@ -6,38 +6,38 @@ import static org.junit.Assert.assertEquals;
 import org.junit.*;
 
 public class TestTicTacToeField {
-    
+
     @Test
     public void write() {
-        final TicTacToeField ttt = new TicTacToeField(3);
+        final TicTacToeField tObj = new TicTacToeField();
 
-        assertEquals(0, ttt.write(0, 0, Cell.O));
-        assertEquals(-1, ttt.write(0, 0, Cell.X));
-        assertEquals(-1, ttt.write(-1, 0, Cell.O));
-        assertEquals(-1, ttt.write(0, -1, Cell.O));
-        assertEquals(-1, ttt.write(3, 0, Cell.X));
-        assertEquals(-1, ttt.write(0, 3, Cell.O));
+        assertEquals(0, tObj.write(0, 0, Cell.O));
+        assertEquals(-1, tObj.write(0, 0, Cell.X));
+        assertEquals(-1, tObj.write(-1, 0, Cell.O));
+        assertEquals(-1, tObj.write(0, -1, Cell.O));
+        assertEquals(-1, tObj.write(3, 0, Cell.X));
+        assertEquals(-1, tObj.write(0, 3, Cell.O));
     }
 
-    @Test 
+    @Test
     public void findMaxLength() {
         TicTacToeField tObj = new TicTacToeField(5);
         int[] expected = new int[5];
-        
-        expected = new int[] {0, 0, 0, 0, 0};
+
+        expected = new int[] { 0, 0, 0, 0, 0 };
         assertArrayEquals(expected, tObj.findMaxLength(Cell.O));
         assertArrayEquals(expected, tObj.findMaxLength(Cell.X));
 
-        expected = new int[] {5, 0, 0, 4, 0};
+        expected = new int[] { 5, 0, 0, 4, 0 };
         assertArrayEquals(expected, tObj.findMaxLength(Cell.EMPTY));
 
         tObj.write(0, 0, Cell.O);
         tObj.write(0, 1, Cell.O);
         tObj.write(1, 0, Cell.X);
         tObj.write(2, 0, Cell.X);
-        expected = new int[] {2, 0, 0, 0, 1};
+        expected = new int[] { 2, 0, 0, 0, 1 };
         assertArrayEquals(expected, tObj.findMaxLength(Cell.O));
-        expected = new int[] {2, 1, 0, 2, 0};
+        expected = new int[] { 2, 1, 0, 2, 0 };
         assertArrayEquals(expected, tObj.findMaxLength(Cell.X));
         tObj.clearAll();
 
@@ -46,9 +46,9 @@ public class TestTicTacToeField {
         tObj.write(1, 0, Cell.X);
         tObj.write(2, 1, Cell.X);
         tObj.write(3, 2, Cell.X);
-        expected = new int[] {2, 0, 0, 1, 1};
+        expected = new int[] { 2, 0, 0, 1, 1 };
         assertArrayEquals(expected, tObj.findMaxLength(Cell.O));
-        expected = new int[] {3, 1, 0, 3, 2};
+        expected = new int[] { 3, 1, 0, 3, 2 };
         assertArrayEquals(expected, tObj.findMaxLength(Cell.X));
         tObj.clearAll();
 
@@ -65,15 +65,36 @@ public class TestTicTacToeField {
         tObj.write(2, 2, Cell.X);
         tObj.write(1, 3, Cell.X);
         tObj.write(0, 4, Cell.X);
-        expected = new int[] {3, 0, 1, 2, 3};
+        expected = new int[] { 3, 0, 1, 2, 3 };
         assertArrayEquals(expected, tObj.findMaxLength(Cell.O));
-        expected = new int[] {5, 4, 0, 0, 4};
+        expected = new int[] { 5, 4, 0, 0, 4 };
         assertArrayEquals(expected, tObj.findMaxLength(Cell.X));
         tObj.clearAll();
     }
 
     @Test 
     public void randomFindWinner() {
-        
+        TicTacToeField tObj = new TicTacToeField(5);
+
+        java.util.Random rand = new java.util.Random();
+        int countCells = 0;
+        Cell type = Cell.X;
+
+        while (countCells != 25) {
+            while (tObj.write(rand.nextInt(5), rand.nextInt(5), type) != 0) {}
+            
+            if (type == Cell.O) 
+                type = Cell.X; 
+            else 
+                type = Cell.O;
+
+            countCells++;
+        }
+
+        // test stability of algorithm
+        assertArrayEquals(tObj.findMaxLength(Cell.O), tObj.findMaxLength(Cell.O));
+        assertArrayEquals(tObj.findMaxLength(Cell.X), tObj.findMaxLength(Cell.X));
+        assertArrayEquals(tObj.findMaxLength(Cell.O), tObj.findMaxLength(Cell.O));
+        assertArrayEquals(tObj.findMaxLength(Cell.X), tObj.findMaxLength(Cell.X));
     }
 }
